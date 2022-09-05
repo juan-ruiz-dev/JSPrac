378aa339eb6ba7e2d1661beeb4551148635ee1d5
Juan Ruiz <jryanruiz@live.com>
2022-09-05 00:38:50 -0700

    just messing around with practice problems

---
+++ javaprac.js
@@ -0,0 +1,76 @@
+// for (let i = 1; i < 11; i++){
+//     if (i % 2 === 0){
+//     console.log(i)
+//     }else {
+//         console.log('Nope')
+//     }
+// }
+
+//create a function to convert kilos to miles
+
+
+// function kiloToMiles (kilo) {
+//     let sum = kilo * .62137;
+//     console.log(`${sum} is the amount of miles.`)
+// }
+
+// kiloToMiles(2342)
+
+//create a function that adds up all elements in an array
+
+
+// let array = [1, 2, 5, -1, 10, 0, 11]
+// let arr2 = [1, 2, .54, 2, -1, 10]
+
+// function sumOfArray (arr) {
+//     let sum = 0;
+//     for(let i = 0; i < arr.length; i++){
+//         sum += arr[i]
+//     }
+//     console.log(sum)
+// }
+// sumOfArray(arr2)
+
+
+// print an array backwards
+
+
+// let probArray = [1, 2, 3, 4, 5, 6, 15, 12, 1]
+// function reverseArray(arr){
+//     ansArray = [];
+//     for(let i = arr.length -1; i >= 0; i--){
+//       ansArray.push(arr[i])
+
+        
+//     }
+//     console.log(ansArray)
+// }
+
+// reverseArray(probArray)
+
+// let array1 = [1, 5, 6, 2, 3]
+
+
+
+// function sArray(arr){
+// let sortedArray = []
+// sortedArray = arr.sort();
+// console.log(sortedArray)
+// }
+
+// sArray(array1)
+
+
+// let array = [-1, 0, 1, 2]
+
+// function noNegatives(arr){
+//     for(let i = 0; i < arr.length; i++){
+//         if(arr[i] < 0){
+//             arr.splice(i, 1)
+//         }
+//     }
+//     console.log(arr)
+// }
+
+// noNegatives(array)
+
